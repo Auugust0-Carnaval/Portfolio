0457b7d59476ed1367f5f8abfae9325b9838b19c
Augusto <augustooliveixa@gmail.com>
2024-06-16 09:00:18 -0300

    creating the page footer

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const footer = () => {
+    return (
+        <footer>
+            <p>Copyright &copy; 2024</p>
+        </footer>
+    )
+}
+
+
+export default footer
+
