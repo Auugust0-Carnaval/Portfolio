bde5e560eddb3a7cbe95a74319954ef768440e7f
Augusto <augustooliveixa@gmail.com>
2024-06-16 09:57:15 -0300

    Styling Background and Setting Components at the Root

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/header';
+import About from './components/about';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div>
+        <Header />
+      </div>
+      <div>
+        <About />
+      </div>
+      <div>
+        <Footer />
+      </div>
+      
     </div>
   );
 }
