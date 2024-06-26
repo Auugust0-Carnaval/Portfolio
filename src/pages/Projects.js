b5d9eb8ca979897815a7f7baf122325ad18d4777
Augusto <augustooliveixa@gmail.com>
2024-06-26 20:22:15 -0300

    Commenting Display Functions and Adding Links

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,53 +1,61 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJava } from '@fortawesome/free-brands-svg-icons';
-import { useState } from 'react';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import { faWindowClose } from '@fortawesome/free-solid-svg-icons';
-import { faUnity } from '@fortawesome/free-brands-svg-icons/faUnity';
-
+import { faJava, faNodeJs, faAngular, faUnity } from '@fortawesome/free-brands-svg-icons';
 
 const Projects = () => {
-    const [showImage, setShowImage] = useState(false);
+    // const [showImage, setShowImage] = useState(false);
 
-    const handleShowImage = () => {
-        setShowImage(true);
-    };
+    // const handleShowImage = () => {
+    //     setShowImage(true);
+    // };
+
+    // const handleCloseImage = () => {
+    //     setShowImage(false);
+    // };
 
     return (
-        <div>
+        <div className="project">
             <div className="row">
-                <div className="col-6" id='unity'>
-                    <FontAwesomeIcon 
-                        icon={faUnity} 
-                        style={{ color: 'white', fontSize: '48px', cursor: 'pointer' }} 
-                        onClick={handleShowImage} 
-                    />
+                <div className="col-1 col-md-auto" id='nodejs'>
+                    <a href='https://github.com/Auugust0-Carnaval/BYTEBOT'>
+                        <FontAwesomeIcon 
+                            icon={faNodeJs} 
+                            style={{ color: 'white', fontSize: '3vw', cursor: 'pointer' }} 
+                            onClick={handleShowImage} 
+                        />
+                    </a>
+                </div>
+                <div className="col-1 col-md-auto" id='angular'>
+                    <a href='https://github.com/Auugust0-Carnaval/TCC-AmoPatasFrontend'>
+                        <FontAwesomeIcon 
+                            icon={faAngular} 
+                            style={{ color: 'white', fontSize: '3vw', cursor: 'pointer' }} 
+                            onClick={handleShowImage} 
+                        />
+                    </a>
+                </div>
+                <div className="col-1 col-md-auto" id='unity'>
+                    <a href='https://github.com/Auugust0-Carnaval/2DGAME'>
+                        <FontAwesomeIcon 
+                            icon={faUnity} 
+                            style={{ color: 'white', fontSize: '3vw', cursor: 'pointer' }} 
+                            onClick={handleShowImage} 
+                        />
+                    </a>
+                </div>
+                <div className="col-1 col-md-auto" id='java'>
+                    <a href='https://github.com/Auugust0-Carnaval/JPA-API'>
+                        <FontAwesomeIcon 
+                            icon={faJava} 
+                            style={{ color: 'white', fontSize: '3vw', cursor: 'pointer' }} 
+                            onClick={handleShowImage} 
+                        />
+                    </a>
                 </div>
             </div>
 
-            {showImage && (
-                <div className="row mt-3">
-                    <div className="col-6">
-                        <div className='row'>
-                            <div className="col-6">
-                                <img
-                                    src="https://i.ytimg.com/vi/9-6ol6_USYg/maxresdefault.jpg" 
-                                    className="img-fluid rounded"
-                                />
-                            </div>
-                            <div className="col-6">
-                                <FontAwesomeIcon icon={faWindowClose} style={{ color: 'white', fontSize: '25px', cursor: 'pointer'}} onClick={() => setShowImage(false)} />
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-            )}
+            
         </div>
-
-        
     );
 };
 
