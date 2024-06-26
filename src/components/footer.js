210d11ddfd406d8356064cc8fa751c64c3b369c9
Augusto <augustooliveixa@gmail.com>
2024-06-26 20:23:12 -0300

    Tag parameter interpolation

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-
-const Footer = () => {
+const Footer = ({text}) => {
     return (
         <footer>
-            <p>Copyright &copy; 2024</p>
+            <p>
+                <a href="https://github.com/Auugust0-Carnaval"><FontAwesomeIcon icon={faGithub} id="github" /></a>
+                {text}
+            </p>
         </footer>
     )
 }
-
 
 export default Footer
 
