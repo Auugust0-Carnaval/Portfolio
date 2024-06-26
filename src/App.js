6817f36c4ed5604ba60f46329772b59d1a3a6ee8
Augusto <augustooliveixa@gmail.com>
2024-06-26 20:20:45 -0300

    Placing New Footer Objects in the Array and Setting Component to the Root

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import React, { useState } from 'react';
 import Header from './components/header';
 import About from './components/about';
 import Projects from './pages/Projects';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import Footer from './components/footer';
-
-
+import Footer from './components/footer';
 
 const App = () => {
   const [language, setLanguage] = useState('en'); //setando valor inicial do idioma da aplicacao como ingles
@@ -19,19 +16,25 @@ const App = () => {
     en: {
       header: "Hi, my name is Augusto",
       about: `Hi everyone! My name is Augusto, and I'm looking for my first opportunity in the IT field, focusing on programming. I have a technical background in Systems Development and have been immersed in this field for a year and a half. I'm proficient in C#, JavaScript, Java, and C. I also have practical experience in assembling and maintaining computers, especially with AMD architecture. I've worked on personal projects that demonstrate my ability to solve complex problems and implement innovative solutions. Despite being introverted, I value teamwork and enjoy interacting with others. I thrive on collaborating, listening to different perspectives, and finding creative solutions.`,
+      footer: " © Copyright &copy; 2024",
     },
     pt: {
       header: "Olá, meu nome é Augusto",
       about: `Olá pessoal! Me chamo Augusto e estou em busca da minha primeira oportunidade na área de Tecnologia da Informação, com foco em programação. Tenho formação técnica em Desenvolvimento de Sistemas e estou imerso nesse universo há um ano e meio. Domino C#, JavaScript, Java e C. Também tenho experiência prática em montagem e manutenção de computadores, especialmente com arquitetura AMD. Tenho trabalhado em projetos pessoais que demonstram minha capacidade de resolver problemas complexos e implementar soluções inovadoras. Apesar de ser introvertido, valorizo o trabalho em equipe e gosto de interagir com outras pessoas. Sou motivado por colaboração, escuta de diferentes perspectivas e busca por soluções criativas.`,
+      footer: " © Todos diretos reservados; 2024",
+      
     },
   };
 
   return (
-    <div className='container'>
-      <Header text={text[language].header} changeLanguage={changeLanguage} />
-      <About text={text[language].about}  />
-      <Projects />
-    </div>
+    <>
+      <div className='container'>
+        <Header text={text[language].header} changeLanguage={changeLanguage} />
+        <About text={text[language].about} />
+        <Projects />
+      </div>
+      <Footer text={text[language].footer} />
+      </>
   );
 };
 
