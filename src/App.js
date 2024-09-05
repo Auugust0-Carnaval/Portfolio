6d779d8d8e2babc2f8b5250af480417f659fd862
Augusto <augustooliveixa@gmail.com>
2024-09-05 18:46:12 -0300

    Placing new component and uploading changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from './components/footer';
 import NavRoot from './components/navRoot';
 import ShowProject from './pages/showProject';
 
+import Works from './pages/works';
+
 const App = () => {
   const [language, setLanguage] = useState('en'); //setando valor inicial do idioma da aplicacao como ingles
 
@@ -20,14 +22,14 @@ const App = () => {
       about: `Hi everyone! My name is Augusto, and I'm looking for my first opportunity in the IT field, focusing on programming. I have a technical background in Systems Development and have been immersed in this field for a year and a half. I'm proficient in C#, JavaScript, Java, and C. I also have practical experience in assembling and maintaining computers, especially with AMD architecture. I've worked on personal projects that demonstrate my ability to solve complex problems and implement innovative solutions. Despite being introverted, I value teamwork and enjoy interacting with others. I thrive on collaborating, listening to different perspectives, and finding creative solutions.`,
       footer: " © Copyright &copy; 2024",
       navRoot: ['ABOUT', 'PROJECTS', 'CONTACT'],
-      showProject: ['Frontend Developer', 'Backend Developer', 'Database Structure', 'I like to develop frontend using JS frameworks like Angular and React. I like the flexibility and interactivity they provide', 'Technologies and tools'],
+      showProject: ['Frontend Developer', 'Backend Developer', 'Database Structure', 'I like to develop frontend using JS frameworks like Angular and React. I like the flexibility and interactivity they provide' , 'Technologies and tools', 'For the backend, I enjoy working with .NET C# and Node.js, using ORM to simplify data management and make development more efficient', 'I work with SQL and MySQL using DML commands to manipulate data, DDL to structure the database, and TCL to manage transactions and ensure data integrity.'],
     },
     pt: {
       header: "Olá, meu nome é Augusto",
       about: `Olá pessoal! Me chamo Augusto e estou em busca da minha primeira oportunidade na área de Tecnologia da Informação, com foco em programação. Tenho formação técnica em Desenvolvimento de Sistemas e estou imerso nesse universo há um ano e meio. Domino C#, JavaScript, Java e C. Também tenho experiência prática em montagem e manutenção de computadores, especialmente com arquitetura AMD. Tenho trabalhado em projetos pessoais que demonstram minha capacidade de resolver problemas complexos e implementar soluções inovadoras. Apesar de ser introvertido, valorizo o trabalho em equipe e gosto de interagir com outras pessoas. Sou motivado por colaboração, escuta de diferentes perspectivas e busca por soluções criativas.`,
       footer: " © Todos diretos reservados; 2024",
       navRoot : ['SOBRE', 'PROJETOS', 'CONTATO'],
-      showProject : ['Desenvolvedor Frontend', 'Desenvolvedor Backend', 'Estrutura de Banco de Dados', 'Curto de desenvolver frontend usando JS frameworks como Angular e React. Gosto da flexibilidade e interatividade que eles proporcionam.', 'Tecnologias e ferramentas'],
+      showProject : ['Desenvolvedor Frontend', 'Desenvolvedor Backend', 'Estrutura de Banco de Dados', 'Curto de desenvolver frontend usando JS frameworks como Angular e React. Gosto da flexibilidade e interatividade que eles proporcionam.', 'Tecnologias e ferramentas','Para o backend, gosto de trabalhar com .NET C# e Node.js, usando ORM para facilitar o gerenciamento de dados e tornar o desenvolvimento mais eficiente.', 'Trabalho com SQL e MySQL usando comandos DML para manipular dados, DDL para estruturar o banco, e TCL para gerenciar transações e garantir a integridade dos dados.'],
       
     },
   };
@@ -38,14 +40,15 @@ const App = () => {
         <NavRoot text={text[language].navRoot} changeLanguage={changeLanguage}/>
         <Header text={text[language].header} changeLanguage={changeLanguage} />
         <About text={text[language].about} />
-        <Projects />
-        
-        
+        <Projects /> 
       </div>
-
       <div className='container-fluid p-0'>
           <ShowProject text={text[language].showProject} changeLanguage={changeLanguage}/>
       </div>
+      <div className='worksContent'>
+        <Works />
+      </div>
+
      
       
       <Footer text={text[language].footer} />
--- src/pages/showProject.js
+++ src/pages/showProject.js
@@ -18,7 +18,7 @@ const ShowProject = ({ text, changeLanguage }) => {
               <h6>{text[3]}</h6>
               <h5 className='subTitle'>{text[4]}</h5>
               <div className='row'>
-                <h6>HTML, CSS ,JS ,React ,Angular ,Bootstrap ,ASP.NET</h6>
+                <h6>HTML, CSS ,JS ,React ,Angular ,Bootstrap ,ASP.NET, Selenium</h6>
               </div>
             
             </div>
@@ -27,8 +27,7 @@ const ShowProject = ({ text, changeLanguage }) => {
             <FontAwesomeIcon icon={faGear} className="icon" id='gearCode'/>
             <h4 className='title-skill'>{text[1]}</h4>
             <div className='content-one'>
-              <h6>
-              Para o backend, gosto de trabalhar com .NET C# e Node.js, usando ORM para facilitar o gerenciamento de dados e tornar o desenvolvimento mais eficiente.</h6>
+              <h6>{text[5]}</h6>
               <h5 className='subTitle'>{text[4]}</h5>
               <div className='row'>
                 <h6>Nodejs, .Net , Postman, Insominia, APIs RESTful, Jest</h6>
@@ -39,7 +38,7 @@ const ShowProject = ({ text, changeLanguage }) => {
             <FontAwesomeIcon icon={faDatabase} className="icon" id='database'/>
             <h4 className='title-skill'>{text[2]}</h4>
             <div className='content-one'>
-              <h6>Trabalho com SQL e MySQL usando comandos DML para manipular dados, DDL para estruturar o banco, e TCL para gerenciar transações e garantir a integridade dos dados.</h6>
+              <h6>{text[6]}</h6>
               <h5 className='subTitle'>{text[4]}</h5>
               <div className='row'>
                 <h6>MySQL Workbench, DBeaver, PostgreSQL, SQL Server Management Studio, Azure Data Studio </h6>
@@ -47,7 +46,6 @@ const ShowProject = ({ text, changeLanguage }) => {
             </div>
           </div>
         </div>
-        
       </div>
       
 
